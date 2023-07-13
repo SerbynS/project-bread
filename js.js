5f23247e87e917cb8ba4eb4a520c246f7b75e643
SerbynS <serbin.sasha1@gmail.com>
2023-07-13 18:04:14 +0300

    modal

---
+++ js.js
@@ -0,0 +1,5 @@
+let btnShop = document.querySelectorAll(".shp");
+btnShop.addEventListener("click", function () {
+   let dropmenu = document.querySelector(".drop-menu");
+   dropmenu.style.display = "flex";
+});
